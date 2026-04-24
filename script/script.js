7fdc7b2512b9fe3361011558eafe6ab63b9a27a3
kearmododragon <ckearney1992@gmail.com>
2026-04-24 23:07:46 +0200

    css set up and basics coded ready for the game state.

--- script/script.js
+++ script/script.js
@@ -4,6 +4,9 @@ const sounds = {
     blue: new Audio("sounds/glass.mp3"),
     green: new Audio("sounds/joke.mp3"),
 }
+const timerEl = document.querySelector(".timer h1")
+const start = document.querySelector(".start-btn")
+
 
 function playSound(color) {
     const sound = sounds[color];
@@ -11,12 +14,38 @@ function playSound(color) {
     sound.play();
     console.log(color)
 }
-
 function startGame() {
-    console.log("start game please")
+    console.log("start game please");
+    start.classList.add("hidden");
+    startTimer();
+    // startTime = null;
+    // timerEl.textContent = "00:00:00";
 }
+function startTimer() {
+    let startTime
+    let timerInterval
+    clearInterval(timerInterval);
+    startTime = Date.now();
+
+    timerInterval = setInterval(() => {
+        const now = Date.now();
+        const diff = now - startTime;
+
+        const minutes = Math.floor(diff / 60000);
+        const seconds = Math.floor((diff % 60000) / 1000);
+        const ms = Math.floor((diff % 1000) / 10);
+
+        timerEl.textContent =
+            `${pad(minutes)}:${pad(seconds)}:${pad(ms)}`;
+    }, 10);
+}
+function pad(value) {
+    return value.toString().padStart(2, "0");
+}
+
+
 document.querySelector(".red").addEventListener("click", () => playSound("red"));
 document.querySelector(".yellow").addEventListener("click", () => playSound("yellow"));
 document.querySelector(".blue").addEventListener("click", () => playSound("blue"));
 document.querySelector(".green").addEventListener("click", () => playSound("green"));
-document.querySelector(".startbtn").addEventListener("click", () => startGame());
+start.addEventListener("click", startGame);
